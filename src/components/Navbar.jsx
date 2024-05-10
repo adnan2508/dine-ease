aeaab0843c35c564ecc30206505e4ddf9a4b2174
adnan2508 <adnandiu2800@gmail.com>
2024-05-10 22:42:12 +0600

    navbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar bg-orange-500 text-primary-content">
+        <button className="btn btn-ghost font-mulish text-xl text-white">DineEase</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
-    return (
-        <div>
-            <h2 className='font-mulish'>This is Home!</h2>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="w-11/12 mx-auto">
+        <h2 className="font-mulish">This is Home!</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
