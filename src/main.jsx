6ce22953988519079cfba68fec4f53dcfc561839
adnan2508 <adnandiu2800@gmail.com>
2024-05-10 22:33:33 +0600

    running succesfully

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Home from './pages/Home.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Home></Home>,
   },
 ]);
 
