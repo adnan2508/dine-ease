3194e5ae1e295102d6a46e37061d3316d8ada51c
adnan2508 <adnandiu2800@gmail.com>
2024-05-11 02:03:05 +0600

    footer added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <div className="w-11/12 mx-auto">
         <h2 className="">This is Home!</h2>
       </div>
+      <Footer></Footer>
     </div>
   );
 };
