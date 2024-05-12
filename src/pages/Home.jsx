bdc9208b55b0f3c042f92b18ec0a44353597f03c
adnan2508 <adnandiu2800@gmail.com>
2024-05-12 22:43:17 +0600

    see all added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Banner from "../components/Banner";
 import { Helmet } from "react-helmet";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -14,6 +15,10 @@ const Home = () => {
 
       <div className="w-11/12 mx-auto mt-5">
         <Banner></Banner>
+        <div className="flex justify-center items-center">
+        <Link to='/allFoods' className="btn my-5 bg-orange-500 text-white font-mulish rounded-3xl px-8 hover:bg-orange-700">See All</Link>
+        </div>
+        
       </div>
 
       <Footer></Footer>
