4bccc3f864bb8ea65f1875dcd87ecc97293de3f3
adnan2508 <adnandiu2800@gmail.com>
2024-05-13 21:21:51 +0600

    design done

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const FoodCard = ({ foods }) => {
-  console.log(foods);
+    const truncatedDescription = foods.description.split(" ").slice(0, 25).join(" ");
   return (
     <div>
       <div className="card card-compact w-96 bg-base-100 shadow-xl mt-5">
@@ -9,14 +9,17 @@ const FoodCard = ({ foods }) => {
           <img
             src={foods.foodImage}
             alt={foods.foodName}
+            className="w-96 h-72"
           />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">{foods.foodName}</h2>
-          <p>Category: {foods.category}</p>
-          <p>{foods.description}</p>
+          <h2 className="card-title text-3xl">{foods.foodName}</h2>
+          <p className="font-semibold text-xl">Category: {foods.category}</p>
+          <p className="font-semibold text-xl">Price: ${foods.price}</p>
+          <p className="font-semibold text-xl">Quantity: {foods.quantity}</p>
+          <p>{truncatedDescription}</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+            <button className="btn bg-orange-500 hover:bg-orange-700 text-white">View Details</button>
           </div>
         </div>
       </div>
