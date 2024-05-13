95e75b07b594290427b3658ca6b761abdf810b0d
adnan2508 <adnandiu2800@gmail.com>
2024-05-14 03:33:06 +0600

    origin added

--- src/pages/MyAddedFood.jsx
+++ src/pages/MyAddedFood.jsx
@@ -53,6 +53,14 @@ const MyAddedFood = () => {
                           <span>Food Name</span>
                         </div>
                       </th>
+                      <th
+                        scope="col"
+                        className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500"
+                      >
+                        <div className="flex items-center gap-x-3">
+                          <span>Country</span>
+                        </div>
+                      </th>
 
                       <th
                         scope="col"
@@ -88,6 +96,9 @@ const MyAddedFood = () => {
                       <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">
                         {food.foodName}
                       </td>
+                      <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">
+                        {food.foodOrigin}
+                      </td>
 
                       <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">
                         ${food.price}
@@ -110,7 +121,8 @@ const MyAddedFood = () => {
                     </td>
                       <td className="px-4 py-4 text-sm whitespace-nowrap">
                         <div className="flex items-center gap-x-6">
-                          <button className="text-gray-500 transition-colors duration-200   hover:text-red-500 focus:outline-none">
+
+                          {/* <button className="text-gray-500 transition-colors duration-200   hover:text-red-500 focus:outline-none">
                             <svg
                               xmlns="http://www.w3.org/2000/svg"
                               fill="none"
@@ -125,7 +137,7 @@ const MyAddedFood = () => {
                                 d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
                               />
                             </svg>
-                          </button>
+                          </button> */}
 
                           <button className="text-gray-500 transition-colors duration-200   hover:text-yellow-500 focus:outline-none">
                             <svg
