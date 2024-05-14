9c0526d236ddadc0d1ed530fb129cb0927219118
adnan2508 <adnandiu2800@gmail.com>
2024-05-14 12:02:12 +0600

    updated successs

--- src/pages/UpdateFood.jsx
+++ src/pages/UpdateFood.jsx
@@ -4,11 +4,46 @@ import Footer from "../components/Footer";
 import { Helmet } from "react-helmet";
 import { AuthContext } from "../provider/AuthProvider";
 import { useLoaderData, useNavigate } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const UpdateFood = () => {
     const {user} = useContext(AuthContext);
+    const navigate = useNavigate();
     const food = useLoaderData();
     console.log(food);
+
+    const handleSubmit= async e => {
+        e.preventDefault();
+        const form = e.target;
+        const foodName = form.food_name.value;
+        const category = form.category.value;
+        const price = form.price.value;
+        const photo = form.photo.value;
+        const description = form.description.value;
+        const updateData={
+            foodName,
+            category,
+            price,
+            photo,
+            description,
+            admin : {
+                email: user?.email,
+                name: user?.displayName,
+                photo: user?.photoURL,
+            },
+        }
+        try{
+            const {data} = await axios.put(`${import.meta.env.VITE_API_URL}foods/${food._id}`, updateData)
+            console.log(data);
+            toast.success('Updated Successfully');
+            navigate('/myAddedFood');
+        } catch(err) {
+            console.log(err);
+            toast.error(err.message);
+        }
+    }
+
   return (
     <div>
         <Helmet><title>DineEase | Update Food</title></Helmet>
@@ -19,7 +54,7 @@ const UpdateFood = () => {
             Update Food
           </h2>
 
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
               <div>
                 <label className="text-gray-700 " htmlFor="food_name">
@@ -28,6 +63,7 @@ const UpdateFood = () => {
                 <input
                   id="food_name"
                   name="food_name"
+                  defaultValue={food.foodName}
                   type="text"
                   className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
                 />
@@ -80,11 +116,12 @@ const UpdateFood = () => {
 
               <div>
                 <label className="text-gray-700 " htmlFor="photo">
-                  Image
+                  Photo
                 </label>
                 <input
                   id="photo"
                   name="photo"
+                  defaultValue={food.foodImage}
                   type="text"
                   className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
                 />
@@ -97,12 +134,14 @@ const UpdateFood = () => {
               <textarea
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
                 name="description"
+                defaultValue={food.description}
                 id="description"
                 cols="30"
               ></textarea>
             </div>
             <div className="flex justify-end mt-6">
-              <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transhtmlForm bg-orange-500 rounded-md hover:bg-orange-700 focus:outline-none focus:bg-orange-500">
+              <button onClick={() => handleSubmit()}
+              className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transhtmlForm bg-orange-500 rounded-md hover:bg-orange-700 focus:outline-none focus:bg-orange-500">
                 Update
               </button>
             </div>
